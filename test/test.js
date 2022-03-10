e060e2b368c0d9dfa67836cc605897160907e031
KorenPapazian <kpapazian@edenschool.fr>
2022-03-10 16:16:41 +0100

    commit content

--- test/test.js
+++ test/test.js
@@ -1,8 +1,34 @@
 var assert = require('assert');
 describe('Array', function () {
-  describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
+});
+
+
+
+describe('TestFunction', function () {
+  it('should return test_controller', function () {
+    assert.equal(TestFunction("TestController"), "test_controller");
+  });
+
+  it('should return movie_and_book', function () {
+    assert.equal(TestFunction("MovieAndBook"), "movie_and_book");
+  });
+
+  it('should return app7_test', function () {
+    assert.equal(TestFunction("App7Test"), "app7_test");
+  });
+
+  it('should return home', function () {
+    assert.equal(TestFunction("Home"), "home");
+  });
+
+  it('should return play-movie', function () {
+    assert.equal(TestFunction("play-movie"), "play-movie");
+  });
+
+  it('should return "1"', function () {
+    assert.equal(TestFunction(1), "1");
   });
 });
